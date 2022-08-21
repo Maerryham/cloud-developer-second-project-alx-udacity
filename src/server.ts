497551a0b64afa7c2b95a3b590fb321a45639673
Mariam Lawal <lawalmariamyetunde@gmail.com>
2022-08-21 20:41:03 +0100

    Correct Status and Validation Issues

--- src/server.ts
+++ src/server.ts
@@ -25,7 +25,7 @@ import path from 'path';
     
     const URL = req.query.image_url
      //    1. validate the image_url query
-    if(validateUrl) {
+    if(!validateUrl) {
        return res.status(500).json({
        status: 'error',
        code: '500',
@@ -48,7 +48,11 @@ import path from 'path';
         fs.readdir(directoryPath, function (err, files) {
           //handling error
           if (err) {
-              return console.log('Unable to scan directory: ' + err, directoryPath);
+              return res.status(500).json({
+                status: 'error',
+                code: '500',
+                message: 'Unable to scan directory: ' + err, directoryPath
+              })
           } 
           // listing all files and adding path to each file
           files = files.map( (file) => {
@@ -59,30 +63,14 @@ import path from 'path';
         });
       
     } catch (e) {
-    return res.status(500).json({
-      status: 'error',
-      code: '500',
-      message: e
-    })
+      return res.status(500).json({
+        status: 'error',
+        code: '500',
+        message: e
+      })
     } 
   });
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-
-  //! END @TODO1
-  
-  // Root Endpoint
+ 
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
     res.send("WELCOME TO MARIAM LAWAL'S IMAGE FILTER APP FOR CLOUD DEVELOPER PROJECT")
